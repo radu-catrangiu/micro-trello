2093fe18794898990fe203e5ebd77fb5e97773df
radu-catrangiu <radu.catrangiu@gmail.com>
2020-05-02 11:38:36 +0300

    Update mongo default config.

--- backend/config.js
+++ backend/config.js
@@ -3,10 +3,10 @@ var env = process.env;
 module.exports = {
     port: env.PORT || 8081,
     mongo: {
-        host: env.MONGO_HOST || 'ds018839.mlab.com',
-        port: env.MONGO_PORT || 18839,
-        database: env.MONGO_DB || 'dss',
-        dbuser: env.MONGO_USER || 'user',
-        dbpassword: env.MONGO_PASS || 'qwerty12'
+        host: env.MONGO_HOST || 'localhost',
+        port: env.MONGO_PORT || 27017,
+        database: env.MONGO_DB || 'microtrello',
+        dbuser: env.MONGO_USER || 'robot',
+        dbpassword: env.MONGO_PASS || 'qwerty123zxc'
     }
 };
